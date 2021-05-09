84b7a0513503caf4ce4de54f2e7c67595fe18caa
Ferdinand Prantl <prantlf@gmail.com>
2021-05-09 18:11:37 +0200

    Add minified ESM package, fix error handling from the typings generation

--- tools/build.js
+++ tools/build.js
@@ -25,6 +25,13 @@ if (prepare || task === 'dist' || watch)
     },
     {
       entryPoints: ['src/index.ts'],
+      outfile: 'dist/index.esm.min.js',
+      format: 'esm',
+      bundle: true,
+      minify: true
+    },
+    {
+      entryPoints: ['src/index.ts'],
       outfile: 'dist/index.umd.js',
       format: 'umd',
       globalName: 'bacom'
@@ -62,7 +69,7 @@ if (task === 'test' || watch)
       format: 'cjs'
     },
     {
-      entryPoints: ['test/example/counter.tsx'],
+      entryPoints: ['test/example/counter.ts'],
       outfile: 'test/example/counter.js',
       format: 'esm',
       bundle: true,
@@ -82,8 +89,8 @@ async function build() {
 function typings() {
   return new Promise((resolve, reject) => {
     const proc = spawn(`${__dirname}/typings`)
-      .on('error', err => console.error(err), reject())
-      .on('close', code => code ? reject() : resolve())
+      .on('error', err => (console.error(err), reject()))
+      .on('exit', code => code ? reject() : resolve())
     proc.stdout.on('data', data => process.stdout.write(data.toString()))
     proc.stderr.on('data', data => process.stderr.write(data.toString()))
   })
